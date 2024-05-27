58081fde074af7067eb8b405627c9e16af340cdf
hermes7144 <kichancho@gmail.com>
2024-05-27 17:56:46 +0900

    feat:tailwind setting

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+  // const { user, login, logout } = useAuthContext();
+  return (
+    <header className='flex justify-between border-b border-gray-300 p-2 font-semibold bg-white'>
+      냉장고
+      <div></div>
+    </header>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import NotFound from './pages/NotFound.tsx';
+import Home from './pages/Home.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [{ index: true, path: '/', element: <Home /> }],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
+
