3a210353b7c1dcba44d146e344262293ad8165a3
hermes7144 <kichancho@gmail.com>
2024-08-06 10:22:07 +0900

    fix:date bug fix

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,16 +1,19 @@
 import dayjs, { Dayjs } from 'dayjs';
 import 'dayjs/locale/ko';
-
+import isoWeek from 'dayjs/plugin/isoWeek';
+dayjs.extend(isoWeek);
 dayjs.locale('ko');
 
-export function formatDate(date?: Dayjs | Date | string | null) {return dayjs(date).format('YYYY-MM-DD')}
+export function formatDate(date?: Dayjs | Date | string | null) {
+  return dayjs(date).format('YYYY-MM-DD');
+}
 
 export const isValidDate = (date: Date | null): boolean => {
   return date !== null && dayjs(date).isValid();
 };
 
 export function getWeekDates(): Dayjs[] {
-  const startOfWeek = dayjs().startOf('week').add(1, 'day');
+  const startOfWeek = dayjs().startOf('isoWeek'); // 월요일부터 시작
   const weekDates = Array.from({ length: 7 }, (_, i) => startOfWeek.add(i, 'day'));
   return weekDates;
 }
