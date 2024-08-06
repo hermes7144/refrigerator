e5be5c894f281d34f67f8663cd96b73e1d1e4050
hermes7144 <kichancho@gmail.com>
2024-08-06 11:57:16 +0900

    feat:datelist set fixed

--- src/components/date/DateList.tsx
+++ src/components/date/DateList.tsx
@@ -3,9 +3,10 @@ import { DateItem } from './DateItem';
 import { DateListProps } from '../../types';
 
 export const DateList: FC<DateListProps> = ({ week, selectedDate, onDateClick }) => (
-
-  <ul className='flex w-full justify-center gap-2 p-2'>
-    {week.map((weekday) => <DateItem key={weekday.format('YYYY-MM-DD')} selected={selectedDate} weekday={weekday} handleClick={onDateClick} />  )}
+  <ul className='flex w-full justify-center gap-2 p-2 fixed top-16 bg-white'>
+    {week.map((weekday) => (
+      <DateItem key={weekday.format('YYYY-MM-DD')} selected={selectedDate} weekday={weekday} handleClick={onDateClick} />
+    ))}
   </ul>
 );
 
--- src/components/meal/MealList.tsx
+++ src/components/meal/MealList.tsx
@@ -4,10 +4,12 @@ import { MealListProps } from '../../types/mealTypes';
 import useMeals from '../../hooks/useMeals';
 
 export const MealList: FC<MealListProps> = ({ week, selectedDate, scrollRefs }) => {
-  const {mealsQuery: { data: meals }} = useMeals(); 
+  const {
+    mealsQuery: { data: meals },
+  } = useMeals();
 
   return (
-    <ul className='flex flex-col w-full md:w-[500px] gap-4 pb-80'>
+    <ul className='flex flex-col w-full md:w-[500px] gap-4 pb-80 mt-20'>
       {week.map((weekday) => {
         return (
           <MealSection
@@ -15,7 +17,7 @@ export const MealList: FC<MealListProps> = ({ week, selectedDate, scrollRefs }) 
             date={selectedDate}
             weekday={weekday}
             meals={meals?.[weekday.format('YYYY-MM-DD')]}
-            scrollRef={(el) => scrollRefs.current[weekday.format('YYYY-MM-DD')] = el}
+            scrollRef={(el) => (scrollRefs.current[weekday.format('YYYY-MM-DD')] = el)}
           />
         );
       })}
